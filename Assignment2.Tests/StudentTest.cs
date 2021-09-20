54f71f3a8baedc1bddcea0f88e1bbb57c54bd490
Robin Ask Rohr Halberg <robin.hal6erg@gmail.com>
2021-09-20 17:42:59 +0200

    Implemented exercise 1 solution

--- Assignment2.Tests/StudentTest.cs
+++ Assignment2.Tests/StudentTest.cs
@@ -6,15 +6,63 @@ namespace Assignment2.Tests
     public class StudentTest
     {
         [Fact]
-        public void ToStringTest()
+        public void StudentToString()
         {
             Student student = new Student(113, "Danny", "Devito", 
                 new DateTime(2020, 8, 1), 
                 new DateTime(2024, 6, 30), 
                 new DateTime(2024, 6, 25));
             
-            string expected = "113, Danny Devito, active, "
-            Assert[]
+            string expected = "113, Danny Devito, Active, 01 Aug 2020 - 30 Jun 2024, Graduation: 25 Jun 2024";
+            Assert.Equal(expected, student.ToString());
+        }
+
+        [Fact]
+        public void StatusNew()
+        {
+            Student student = new Student(113, "Danny", "Devito", 
+                new DateTime(2045, 8, 1), 
+                new DateTime(2049, 4, 30), 
+                null);
+
+            Status expected = Status.New;
+            Assert.Equal(expected, student.status);
+        }
+
+        [Fact]
+        public void StatusActive()
+        {
+            Student student = new Student(113, "Danny", "Devito", 
+                new DateTime(2020, 8, 1), 
+                new DateTime(2024, 6, 30), 
+                new DateTime(2024, 6, 25));
+
+            Status expected = Status.Active;
+            Assert.Equal(expected, student.status);
+        }
+
+        [Fact]
+        public void StatusDropout()
+        {
+            Student student = new Student(113, "Danny", "Devito", 
+                new DateTime(2019, 8, 1), 
+                new DateTime(2020, 4, 30), 
+                null);
+
+            Status expected = Status.Dropout;
+            Assert.Equal(expected, student.status);
+        }
+
+        [Fact]
+        public void StatusGraduated()
+        {
+            Student student = new Student(113, "Danny", "Devito", 
+                new DateTime(2019, 8, 1), 
+                new DateTime(2020, 6, 30), 
+                new DateTime(2020, 6, 30));
+
+            Status expected = Status.Graduated;
+            Assert.Equal(expected, student.status);
         }
     }
 }
--- Assignment2/Student.cs
+++ Assignment2/Student.cs
@@ -1,36 +1,40 @@
+using System;
+
+public enum Status { New, Active, Dropout, Graduated }
+
 public class Student{
+    
+
     private int _id;
     public int id { get { return _id; } }
     public string givenName { get; set; }
     public string surname { get; set; }
-    public const enum status{
-        new,
-        active,
-        dropout,
-        graduated
-    }
-    public status newbie {
-        get {
-
-        } set {
-
-        }
-    }
+    private Status _status;
+    public Status status { get { setStatus(); return _status; } }
     public DateTime startDate{get; set;}
     public DateTime endDate{get; set;}
-    public DateTime graduationDate{get; set;}
-    
-    public Student(int id, string givenName, string surname, DateTime startDate, DateTime endDate, DateTime graduationDate) {
+    public DateTime? graduationDate{get; set;}
+
+
+    public Student(int id, string givenName, string surname, DateTime startDate, DateTime endDate, DateTime? graduationDate) {
         _id = id;
         this.givenName = givenName;
         this.surname = surname;
         this.startDate = startDate;
         this.endDate = endDate;
         this.graduationDate = graduationDate;
+        setStatus();
     }
 
+    private void setStatus() {
+        if (startDate > DateTime.Now) _status = Status.New;
+        else if (startDate < DateTime.Now && endDate > DateTime.Now) _status = Status.Active;
+        else if (endDate < DateTime.Now && graduationDate == null) _status = Status.Dropout;
+        else if (graduationDate < DateTime.Now) _status = Status.Graduated;
+        else _status = Status.Active;
+    }
 
-    public string ToString() {
-        return id + ", " + givenName + " " + surname + ", " + status + ", " + startDate.ToString("yyyy-MM-dd") + " - " + endDate.ToString("yyyy-MM-dd") + ", Graduation: " + graduationDate.ToString("yyyy-MM-dd");
+    public override string ToString() {
+        return id + ", " + givenName + " " + surname + ", " + status + ", " + startDate.ToString("dd MMM yyyy") + " - " + endDate.ToString("dd MMM yyyy") + ", Graduation: " + graduationDate?.ToString("dd MMM yyyy");
     }
 }
